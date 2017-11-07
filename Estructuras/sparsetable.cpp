bf19d344bbd331297b00ea2801f3c1a7e2af4f96
Alexander Irribarra <airribarra@udec.cl>
2017-11-07 19:44:20 -0300

    AAAAA

--- Estructuras/sparsetable.cpp
+++ Estructuras/sparsetable.cpp
@@ -1,42 +1,30 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-using namespace std;
-
-#define MAX_N 1000                           // adjust this value as needed
-#define LOG_TWO_N 10            // 2^10 > 1000, adjust this value as needed
-
-class RMQ {                                          // Range Minimum Query
-private:
-  int _A[MAX_N], SpT[MAX_N][LOG_TWO_N];
-public:
-  RMQ(int n, int A[]) {    // constructor as well as pre-processing routine
-    for (int i = 0; i < n; i++) {
-      _A[i] = A[i];
-      SpT[i][0] = i; // RMQ of sub array starting at index i + length 2^0=1
+struct SparseTable{
+    //se puede cambiar gcd por funcion f que cumpla
+    // f(a,b,c)=f(a,f(b,c))=f(f(a,b),c)
+    int table[10000][17],n;
+    int ZERO;   // f(ZERO,x)=f(x,ZERO)=x
+    vector<int> A;
+    SparseTable(vector<int> &_A){
+        A=_A;
+        ZERO=0;
+        n=A.size();
+        for(int i=0;i<n;i++){
+            table[i][0]=A[i];
+        }
+        for(int j=1;j<=17;j++){
+            for(int i=0;i<=n-(1<<j);i++){
+                table[i][j]=__gcd(table[i][j-1],table[i+(1<<(j-1))][j-1]);
+            }
+        }
     }
-    // the two nested loops below have overall time complexity = O(n log n)
-    for (int j = 1; (1<<j) <= n; j++) // for each j s.t. 2^j <= n, O(log n)
-      for (int i = 0; i + (1<<j) - 1 < n; i++)    // for each valid i, O(n)
-        if (_A[SpT[i][j-1]] < _A[SpT[i+(1<<(j-1))][j-1]])            // RMQ
-          SpT[i][j] = SpT[i][j-1];    // start at index i of length 2^(j-1)
-        else                  // start at index i+2^(j-1) of length 2^(j-1)
-          SpT[i][j] = SpT[i+(1<<(j-1))][j-1];
-  }
-
-  int query(int i, int j) {
-    int k = (int)floor(log((double)j-i+1) / log(2.0));    // 2^k <= (j-i+1)
-    if (_A[SpT[i][k]] <= _A[SpT[j-(1<<k)+1][k]]) return SpT[i][k];
-    else                                         return SpT[j-(1<<k)+1][k];
-} };
-
-int main() {
-  // same example as in chapter 2: segment tree
-  int n = 7, A[] = {18, 17, 13, 19, 15, 11, 20};
-  RMQ rmq(n, A);
-  for (int i = 0; i < n; i++)
-    for (int j = i; j < n; j++)
-      printf("RMQ(%d, %d) = %d\n", i, j, rmq.query(i, j));
-
-  return 0;
-}
+    int query(int L,int R){
+        int answer=ZERO;
+        for(int j=17;j>=0;j--){
+            if(L+(1<<j)-1<=R){
+                answer=__gcd(answer,table[L][j]);
+                L+=1<<j;
+            }
+        }
+        return answer;
+    }
+};
